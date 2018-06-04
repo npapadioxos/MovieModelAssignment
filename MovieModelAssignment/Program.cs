7f7bd0cc99b7e42edf7fcfbe00c71575057f0711
NP <npapadiochos@motivian.com>
2018-06-04 19:06:01 +0300

    Working Example

--- MovieModelAssignment/Program.cs
+++ MovieModelAssignment/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 namespace MovieModelAssignment
 {
+	/// <summary>
+	/// Crating the Movie Class
+	/// </summary>
 	public class Movie
 	{
 		#region Public Properties
@@ -21,6 +24,12 @@ namespace MovieModelAssignment
 	{
 		#region Private Methods
 
+		/// <summary>
+		/// Calculates the overall rating of the movie, based on user reviews
+		/// </summary>
+		/// <param name="movie"></param>
+		/// <param name="randomReview"></param>
+		/// <returns>double</returns>
 		private static double CalculateRating(Movie movie,Random randomReview)
 		{
 			for(int i = 0; i < 100; i++)
@@ -30,21 +39,82 @@ namespace MovieModelAssignment
 			return movie.review.Average();
 		}
 
+		/// <summary>
+		/// Creates the List of Movies
+		/// </summary>
+		/// <param name="moviesTitleList"></param>
+		/// <param name="moviesActorsList"></param>
+		/// <returns>Lists of Movies</returns>
+		private static List<Movie> CreateMovieList(List<string> moviesTitleList,List<string> moviesActorsList)
+		{
+			int index = 0;
+			Random random = new Random();
+			List<Movie> movieList = new List<Movie>();
+
+			foreach(string movieTitle in moviesTitleList)
+			{
+				Movie movie = new Movie()
+				{
+					Title = movieTitle,
+					TicketReceipts = random.Next(1,1000)
+				};
+
+				for(int i = 0; i < 3; i++)
+				{
+					index = random.Next(moviesActorsList.Count);
+					if(!movie.actors.Contains(moviesActorsList[index]))
+					{
+						movie.actors.Add(moviesActorsList[index]);
+					}
+				}
+
+				movie.Rating = CalculateRating(movie,random);
+
+				movieList.Add(movie);
+			}
+
+			return movieList;
+		}
+
 		private static void Main(string[] args)
 		{
-			Random randomReview = new Random();
+			List<string> moviesTitleList = new List<string>();
+			moviesTitleList.AddRange(new string[] { "Blue Skyes","The Red Valley","Beyond Truth","The Dark Spade","Lonely Trip","The Ancestors","Protesters","The Rise Of The Last Empire","2099: The Final Call","Work Junkies","The Man Without Ego: Me","Identity: Graves Decorator","The Woman Who Did Not Exist","The Unknowners","Nicknamer: A Biography","Escape From Penthouse","The Life Of An Ant" });
 
-			Movie IronMan = new Movie()
+			List<string> moviesActorsList = new List<string>();
+			moviesActorsList.AddRange(new string[] { "Jonathan Clay","Antony Bayman","Marianna Johnson","Patrick Long","Tamara May","Ray Lean","Tim Tom","Eugene Fox","Eric Ericson","Lenny Linux","Scala Johansson","Mona Liza HardDrive","William Windows","Scotty Man","Bernard Cook","Yolanda Pearson","Steve Papas","Gus G" });
+
+			List<Movie> movieList = new List<Movie>();
+			movieList = CreateMovieList(moviesTitleList,moviesActorsList);
+
+			PrintMovies(movieList);
+		}
+
+		/// <summary>
+		/// Prints TOP 10 movies based on users reviews
+		/// </summary>
+		/// <param name="movieList"></param>
+		private static void PrintMovies(List<Movie> movieList)
+		{
+			Console.WriteLine("MovieModelAssignment 06/2018\n\n*** Top 10 Movies Based on User Reviews ***");
+
+			//Sorting the movieList based on the Rating (Descending order - best rating appears 1st)
+			List<Movie> SortedList = movieList.OrderByDescending(s => s.Rating).ToList();
+
+			//Keep only the first 10 elements of the SortedList for presentation
+			IEnumerable<Movie> FinalList = SortedList.Take(10);
+
+			foreach(Movie movie in FinalList)
 			{
-				Title = "Ironman",
-				TicketReceipts = 585.2
-			};
+				Console.WriteLine("\n\nTitle = {0}\nRating = {1} %\nTicketReceipts = {2} (million USD)",movie.Title,movie.Rating,movie.TicketReceipts);
 
-			IronMan.Rating = CalculateRating(IronMan,randomReview);
+				Console.Write("Actors = ");
 
-			IronMan.actors.AddRange(new string[] { "actor1","actor2","actor3" });
-
-			Console.WriteLine("Title = {0}\nTicketReceipts= {1}\nActors = {2}\nRating = {3}",IronMan.Title,IronMan.TicketReceipts,IronMan.actors,IronMan.Rating);
+				foreach(String actor in movie.actors)
+				{
+					Console.Write(actor + " \t ");
+				}
+			}
 		}
 	}
 }
